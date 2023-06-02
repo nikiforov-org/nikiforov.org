3d32bb9dfc9b938dcacaf8924680565a1fcfbd15
Evgenii <e@nikiforov.org>
2023-06-02 20:40:53 +0300

    optimize code

--- script.js
+++ script.js
@@ -3,15 +3,15 @@ const buttons = document.querySelectorAll(`.contacts-button`);
 buttons.forEach(button => {
     button.addEventListener(`click`, e => {
         e.preventDefault();
-        const action = button.getAttribute(`title`);
+        const action = button.id;
         switch (action) {
-            case `Telegram`:
+            case `telegram`:
                 window.location.href = `https://t.me/evgeniy_nikiforov`;
                 break;
-            case `WhatsApp`:
+            case `whatsapp`:
                 window.location.href = `https://wa.me/79859071494`;
                 break;
-            case `Email`:
+            case `email`:
                 window.location.href = `mailto:e@${window.location.hostname}`;
                 break;    
             default:
