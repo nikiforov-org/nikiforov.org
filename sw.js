c92d4bdfd1e7a59f3d8e138e2c440aed15fab250
nikiforov-org <e@nikiforov.org>
2023-08-05 01:53:05 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -12,4 +12,14 @@ const fetchListener = e => {
       })
   )
 };
+
 self.addEventListener('fetch', fetchListener);
+
+self.addEventListener('activate', e => {
+    // Important! Start processing fetches for all clients immediately.
+    //
+    // MDN: "When a service worker is initially registered, pages won't use it
+    // until they next load. The claim() method causes those pages to be
+    // controlled immediately."
+    e.waitUntil(clients.claim());
+});
